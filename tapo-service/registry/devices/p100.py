82b662af8c45271eaf43a6dbfe58b331672951f6
Ruben <rwoldhui@avans.nl>
2021-01-06 16:04:30 +0100

    [Added] a timeout to refresh the tokens

--- tapo-service/registry/devices/p100.py
+++ tapo-service/registry/devices/p100.py
@@ -1,3 +1,4 @@
+import time
 from typing import Dict
 
 from drivers.p100.p100 import P100
@@ -8,30 +9,46 @@ class TapoP100(TapoDevice):
     def __init__(self, ip_address: str, email: str, password: str, device_type: str):
         super().__init__(ip_address, email, password, device_type)
         self.p100 = P100(self.ip_address)
+        self.__connect__()
 
+    def __connect__(self):
         try:
             self.p100.handshake()
             self.p100.login_request(self.email, self.password)
             self.initialized = True
-        except:
+        except Exception as e:
             print("error thrown when connecting to device at: {}".format(self.ip_address))
+            print(e)
             self.initialized = False
 
+        self.timeout = time.time()
+
     def turn_on(self):
-        self.p100.change_state(1, "88-00-DE-AD-52-E1")
+        if not self.initialized or time.time() - self.timeout > 60:
+            self.__connect__()
+
+        if self.initialized:
+            self.p100.change_state(1, "88-00-DE-AD-52-E1")
 
     def turn_off(self):
-        self.p100.change_state(0, "88-00-DE-AD-52-E1")
+        if not self.initialized or time.time() - self.timeout > 60:
+            self.__connect__()
+
+        if self.initialized:
+            self.p100.change_state(0, "88-00-DE-AD-52-E1")
 
     def get_device_info(self) -> Dict[str, str]:
         device_info = {}
+        if not self.initialized or time.time() - self.timeout > 60:
+            self.__connect__()
+
         if self.initialized:
             device_info = self.p100.get_state()
         return device_info
 
     def get_device_name(self) -> str:
         if self.initialized and self.device_name != "":
-            self.device_name = self.p100.get_state()
+            self.device_name = ""
         return self.device_name
 
     def get_device_type(self) -> str:
