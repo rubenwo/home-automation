7e55cdf48473a68d9c39aaf3e015a390e3039155
Ruben <rwoldhui@avans.nl>
2021-01-24 13:27:34 +0100

    [Added] multiple modes & colour correction

--- led-strip-controller/src/main.cpp
+++ led-strip-controller/src/main.cpp
@@ -4,18 +4,71 @@
 #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
+#include <array>
+#include <vector>
+
+#define HTTP_PORT 80
+#define LED_COUNT 150
+#define LED_STRIP_DATA_PIN 4
 
 const char *ssid = "";
 const char *password = "";
 
-WebServer server(80);
+WebServer server(HTTP_PORT);
 
-Adafruit_NeoPixel led_strip(150, 4, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel led_strip(LED_COUNT, LED_STRIP_DATA_PIN, NEO_GRB + NEO_KHZ800);
+
+struct rgb
+{
+  int r, g, b;
+};
+struct hsv
+{
+  int h, s, v;
+};
+
+int red, green, blue;
+int hue, value, saturation;
+std::vector<rgb> gradient_rgb_config;
+std::vector<hsv> gradient_hsv_config;
+enum Mode
+{
+  INVALID = -1,
+  SINGLE_COLOR_RGB = 0,
+  SINGLE_COLOR_HSV = 1,
+  GRADIENT_RGB = 2,
+  GRADIENT_HSV = 3,
+  ANIMATION_RGB = 4,
+  ANIMATION_HSV = 5,
+};
+
+Mode mode = Mode::SINGLE_COLOR_RGB;
+
+Mode from_string(const String &str)
+{
+  if (str == "SINGLE_COLOR_RGB")
+    return Mode::SINGLE_COLOR_RGB;
+  else if (str == "SINGLE_COLOR_HSV")
+    return Mode::SINGLE_COLOR_HSV;
+  else if (str == "GRADIENT_RGB")
+    return Mode::GRADIENT_RGB;
+  else if (str == "GRADIENT_HSV")
+    return Mode::GRADIENT_HSV;
+  else if (str == "ANIMATION_RGB")
+    return Mode::ANIMATION_RGB;
+  else if (str == "ANIMATION_HSV")
+    return Mode::ANIMATION_HSV;
+
+  return Mode::INVALID;
+}
+
+unsigned long timer;
+int animation_speed = 10;
 
 // JSON data buffer
 StaticJsonDocument<500> jsonDocument;
 char buffer[500];
-bool on = false;
+
 void setup()
 {
   Serial.begin(460800);
@@ -40,7 +93,6 @@ void setup()
     serializeJson(jsonDocument, buffer);
     server.send(200, "application/json", buffer);
   });
-  pinMode(2, OUTPUT);
   server.on("/led", HTTP_POST, []() {
     Serial.println("/led");
     if (server.hasArg("plain") == false)
@@ -50,22 +102,120 @@ void setup()
       jsonDocument["error_message"] = "body is not valid";
       serializeJson(jsonDocument, buffer);
       server.send(422, "application/json", buffer);
+      return;
     }
 
     String body = server.arg("plain");
     deserializeJson(jsonDocument, body);
 
     // Get RGB components
-    int red = jsonDocument["red"];
-    int green = jsonDocument["green"];
-    int blue = jsonDocument["blue"];
-    Serial.printf("R: %d, G: %d, B: %d\n", red, green, blue);
+    String json_mode = jsonDocument["mode"];
+    mode = from_string(json_mode);
+    JsonArray json_config;
 
-    for (auto i = 0; i < led_strip.numPixels(); i++)
+    switch (mode)
     {
-      led_strip.setPixelColor(i, red, green, blue);
+    case Mode::SINGLE_COLOR_RGB:
+      red = jsonDocument["red"];
+      green = jsonDocument["green"];
+      blue = jsonDocument["blue"];
+
+      led_strip.fill(led_strip.gamma32(led_strip.Color(red, green, blue)), 0, led_strip.numPixels() - 1);
+      led_strip.show();
+      break;
+    case Mode::SINGLE_COLOR_HSV:
+      hue = jsonDocument["hue"];
+      saturation = jsonDocument["saturation"];
+      value = jsonDocument["value"];
+
+      led_strip.fill(led_strip.gamma32(led_strip.ColorHSV(hue, saturation, value)), 0, led_strip.numPixels() - 1);
+      led_strip.show();
+      break;
+    case Mode::GRADIENT_RGB:
+    {
+      json_config = jsonDocument["config"];
+      gradient_rgb_config.clear();
+      for (auto i = 0; i < json_config.size(); i++)
+      {
+        gradient_rgb_config.push_back(rgb{
+            json_config[i]["red"],
+            json_config[i]["green"],
+            json_config[i]["blue"],
+        });
+      }
+      int step_size = LED_COUNT / gradient_rgb_config.size();
+      int rgb_index = 0;
+      for (auto i = 0; i < led_strip.numPixels(); i++)
+      {
+        if (i / (rgb_index + 1) >= step_size)
+          rgb_index++;
+        uint32_t col = led_strip.gamma32(led_strip.Color(
+            gradient_rgb_config[rgb_index].r,
+            gradient_rgb_config[rgb_index].g,
+            gradient_rgb_config[rgb_index].b));
+        led_strip.setPixelColor(i, col);
+
+        // Serial.printf("Index: %d, RGB_INDEX: %d, Step Size: %d, R: %d, G: %d, B: %d\n", i, rgb_index, step_size, gradient_rgb_config[rgb_index].r, gradient_rgb_config[rgb_index].g, gradient_rgb_config[rgb_index].b);
+      }
+      led_strip.show();
     }
-    led_strip.show();
+    break;
+    case Mode::GRADIENT_HSV:
+    {
+      json_config = jsonDocument["config"];
+      gradient_hsv_config.clear();
+      for (auto i = 0; i < json_config.size(); i++)
+      {
+        gradient_hsv_config.push_back(hsv{
+            json_config[i]["hue"],
+            json_config[i]["saturation"],
+            json_config[i]["value"],
+        });
+      }
+      int step_size = LED_COUNT / gradient_hsv_config.size();
+      int hsv_index = 0;
+
+      for (auto i = 0; i < led_strip.numPixels(); i++)
+      {
+        if (i / (hsv_index + 1) >= step_size)
+          hsv_index++;
+        uint32_t col = led_strip.gamma32(led_strip.ColorHSV(
+            gradient_hsv_config[hsv_index].h,
+            gradient_hsv_config[hsv_index].s,
+            gradient_hsv_config[hsv_index].v));
+        led_strip.setPixelColor(i, col);
+      }
+      led_strip.show();
+    }
+    break;
+    case Mode::ANIMATION_RGB:
+      red = jsonDocument["red"];
+      green = jsonDocument["green"];
+      blue = jsonDocument["blue"];
+      animation_speed = jsonDocument["animation_speed"];
+      break;
+    case Mode::ANIMATION_HSV:
+      hue = jsonDocument["hue"];
+      saturation = jsonDocument["saturation"];
+      value = jsonDocument["value"];
+      animation_speed = jsonDocument["animation_speed"];
+      break;
+    case Mode::INVALID:
+      Serial.println("ERROR in /led");
+      jsonDocument.clear();
+      jsonDocument["error_message"] = "mode is not valid";
+      serializeJson(jsonDocument, buffer);
+      server.send(422, "application/json", buffer);
+      return;
+    default:
+      Serial.println("ERROR in /led");
+      jsonDocument.clear();
+      jsonDocument["error_message"] = "mode is not valid";
+      serializeJson(jsonDocument, buffer);
+      server.send(422, "application/json", buffer);
+      return;
+    }
+
     jsonDocument.clear();
     jsonDocument["message"] = "successfully changed colour of the led";
     serializeJson(jsonDocument, buffer);
@@ -88,42 +238,57 @@ void setup()
   });
 
   server.begin();
-
   // init led strip
   led_strip.begin();
-  int r, g, b;
-  r = 245;
-  g = 149;
-  b = 24;
-  for (auto i = 0; i < led_strip.numPixels(); i++)
-  {
-    led_strip.setPixelColor(i, r, g, b);
-  }
+  led_strip.fill(led_strip.gamma32(led_strip.Color(200, 100, 0)), 0, led_strip.numPixels() - 1);
   led_strip.show();
-
-  // announce online
-  // HTTPClient http;
-  // http.begin("http://192.168.2.135/api/v1/new_id");
-  // int httpResponseCode = http.GET();
-  // String payload = "{}";
-  // if (httpResponseCode > 0)
-  // {
-  //   Serial.printf("HTTP Response code: %d\n", httpResponseCode);
-  //   payload = http.getString();
-  // }
-  // else
-  // {
-  //   Serial.printf("Error code: %d\n", httpResponseCode);
-  // }
-  // http.end();
-  // Serial.println(payload);
-
-  // deserializeJson(jsonDocument, payload);
-  // String val = jsonDocument["id"];
-  // Serial.println(val);
+  timer = millis();
 }
+
+int current_led = 0;
 
 void loop()
 {
   server.handleClient();
+  switch (mode)
+  {
+  case Mode::ANIMATION_RGB:
+  {
+    if (millis() - timer > animation_speed)
+    {
+      if (current_led > 0)
+        led_strip.setPixelColor(current_led - 1, 0, 0, 0);
+
+      if (current_led >= led_strip.numPixels())
+        current_led = 0;
+      uint32_t col = led_strip.gamma32(led_strip.Color(red, green, blue));
+      led_strip.setPixelColor(current_led, col);
+      led_strip.show();
+      current_led++;
+
+      timer = millis();
+    }
+  }
+  break;
+  case Mode::ANIMATION_HSV:
+  {
+    if (millis() - timer > animation_speed)
+    {
+      if (current_led > 0)
+        led_strip.setPixelColor(current_led - 1, 0, 0, 0);
+
+      if (current_led >= led_strip.numPixels())
+        current_led = 0;
+      uint32_t col = led_strip.gamma32(led_strip.ColorHSV(hue, saturation, value));
+      led_strip.setPixelColor(current_led, col);
+      led_strip.show();
+      current_led++;
+
+      timer = millis();
+    }
+  }
+  break;
+  default:
+    break;
+  }
 }
