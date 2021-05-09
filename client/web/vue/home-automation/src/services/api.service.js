23946f378fdfde073d79e11fdd626a4ccf38f429
Ruben <rubenwoldhuis@gmail.com>
2021-05-09 21:29:54 +0200

    [Added] showing lat lng

--- client/web/vue/home-automation/src/services/api.service.js
+++ client/web/vue/home-automation/src/services/api.service.js
@@ -15,9 +15,49 @@ export default () => {
   http.interceptors.response.use(undefined, async (err) => {
     console.log("Hi There", err.response.status);
     if (err.response.status === 401) {
-      store.dispatch("auth/logout");
+      store.dispatch("auth/refreshToken");
+      // store.dispatch("auth/logout");
     }
     return err.response;
   });
   return http;
 };
+
+// let isAlreadyFetchingAccessToken = false
+// let subscribers = []
+//
+// function onAccessTokenFetched(access_token) {
+//   subscribers = subscribers.filter(callback => callback(access_token))
+// }
+//
+// function addSubscriber(callback) {
+//   subscribers.push(callback)
+// }
+//
+// axios.interceptors.response.use(function (response) {
+//   return response
+// }, function (error) {
+//   const { config, response: { status } } = error
+//   const originalRequest = config
+//
+//   if (status === 401) {
+//     if (!isAlreadyFetchingAccessToken) {
+//       isAlreadyFetchingAccessToken = true
+//
+//       // instead of this store call you would put your code to get new token
+//       store.dispatch(fetchAccessToken()).then((access_token) => {
+//         isAlreadyFetchingAccessToken = false
+//         onAccessTokenFetched(access_token)
+//       })
+//     }
+//
+//     const retryOriginalRequest = new Promise((resolve) => {
+//       addSubscriber(access_token => {
+//         originalRequest.headers.Authorization = 'Bearer ' + access_token
+//         resolve(axios(originalRequest))
+//       })
+//     })
+//     return retryOriginalRequest
+//   }
+//   return Promise.reject(error)
+// })
