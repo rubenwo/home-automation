482ac895efe47b544bf6c402e330093a74bb594e
ruben <rwoldhui@avans.nl>
2021-05-18 10:25:53 +0200

    [Added] headers support to http calls in JS VM

--- services/registry-service/cmd/api/main.go
+++ services/registry-service/cmd/api/main.go
@@ -6,7 +6,47 @@ import (
 	"net/http"
 )
 
+//var js = `
+//console.log("hello world");
+//resp = HttpGet("https://homeautomation.rubenwoldhuis.nl/api/v1/devices");
+//console.log(resp);
+//
+//data = {
+//	username:"",
+//	password:""
+//}
+//
+//resp = HttpPost("https://homeautomation.rubenwoldhuis.nl/auth/login", data);
+//
+//loginResp = JSON.parse(resp);
+//
+//console.log(loginResp.authorization_token);
+//
+//headers = {
+//	Authorization:"Bearer " + loginResp.authorization_token
+//}
+//
+//resp = HttpGet("https://homeautomation.rubenwoldhuis.nl/api/v1/devices", headers);
+//console.log(resp);
+//
+//`
+
+
 func main() {
+	//vm := otto.New()
+	//
+	//_ = vm.Set("HttpGet", script.HttpGet)
+	//_ = vm.Set("HttpPost", script.HttpPost)
+	//_ = vm.Set("HttpDelete", script.HttpDelete)
+	//_ = vm.Set("HttpPut", script.HttpPut)
+	//
+	//res, err := vm.Run(js)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//log.Println(res)
+
+
 	cfg, err := registry.LoadConfigFromPath("./config.json")
 	if err != nil {
 		log.Fatal(err)
