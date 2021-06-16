d1bd6997dc4c93741eef4df1ba263f1f96061dd5
ruben <rwoldhui@avans.nl>
2021-06-16 14:00:40 +0200

    [Added] logs in frontend

--- client/web/home-automation/src/views/Routine.vue
+++ client/web/home-automation/src/views/Routine.vue
@@ -10,46 +10,66 @@
                 Script: [{{action.script}}]
             </li>
         </ul>
+
+        <div>
+            <h4>Logs</h4>
+            <ul v-if="logs !== null">
+                <li v-bind:key="'logs-' +index" v-for="(log, index) in logs">
+                    [{{log.logged_at}}] - [{{log.message}}]
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
-  import RoutinesService from "../services/routines.service";
+    import RoutinesService from "../services/routines.service";
 
-  export default {
-    name: "Routine",
-    data() {
-      return {
-        routine: {
-          id: 0,
-          name: "",
-          is_active: false,
-          trigger: {
-            cron_expr: "",
-            type: 0
-          },
-          actions: [
-            {
-              script: "",
-              data: null,
-              method: "",
-              addr: ""
+    export default {
+        name: "Routine",
+        data() {
+            return {
+                routine: {
+                    id: 0,
+                    name: "",
+                    is_active: false,
+                    trigger: {
+                        cron_expr: "",
+                        type: 0
+                    },
+                    actions: [
+                        {
+                            script: "",
+                            data: null,
+                            method: "",
+                            addr: ""
+                        }
+                    ]
+                },
+                logs: [
+                    {
+                        logged_at: "",
+                        message: ""
+                    }
+                ]
             }
-          ]
-        }
-      }
-    },
-    async created() {
-      this.id = this.$route.params.id;
+        },
+        async created() {
+            this.id = this.$route.params.id;
 
-      const res = await RoutinesService.fetchRoutine(this.id);
-      console.log(res);
-      this.routine = res.routine;
-      console.log(this.routine);
-    },
-    mounted() {
-    },
-  }
+            const res = await RoutinesService.fetchRoutine(this.id);
+            console.log(res);
+            this.routine = res.routine;
+            console.log(this.routine);
+
+            const logsRes = await RoutinesService.fetchLogsForId(this.id);
+            console.log(logsRes);
+            this.logs = logsRes.logs;
+            console.log(this.logs);
+        },
+        mounted() {
+        },
+    }
 </script>
 
 <style scoped>
