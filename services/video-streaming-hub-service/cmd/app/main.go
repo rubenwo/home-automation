88c2dea41ad44d2f571fd0781c53f99e49fd7702
Ruben <rubenwoldhuis@gmail.com>
2021-07-22 16:33:47 +0200

    [Added] mjpeg video streaming

--- services/video-streaming-hub-service/cmd/app/main.go
+++ services/video-streaming-hub-service/cmd/app/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"github.com/rubenwo/home-automation/services/video-streaming-hub-service/internal/app"
 	"log"
+	"time"
 )
 
 func main() {
 	if err := app.Run(
 		app.Config{
+			//DatabaseAddr: "192.168.2.135:5432",
 			DatabaseAddr: "postgres.default.svc.cluster.local:5432",
 			//DatabaseAddr:     "localhost:5432",
 			DatabaseUser:     "user",
 			DatabasePassword: "password",
 			DatabaseName:     "camera_database",
 			ApiAddr:          ":80",
+			StreamInterval:   time.Millisecond * 30,
 		}); err != nil {
 		log.Fatal(err)
 	}
