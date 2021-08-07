ad1627f4df7f57a8c4d504a5ce890afce9ccfd11
Ruben <rubenwoldhuis@gmail.com>
2021-08-07 18:46:39 +0200

    [Added] error message when device could not be loaded

--- client/web/home-automation/src/components/AppCard.vue
+++ client/web/home-automation/src/components/AppCard.vue
@@ -1,8 +1,9 @@
 <template>
     <div v-if="this.state === 'loaded'">
-        <b-card
-                v-bind:title="name"
-                style="
+        <div v-if="this.dev !== null">
+            <b-card
+                    v-bind:title="name"
+                    style="
         max-width: 540px;
         min-width: 200px;
         min-height: 425px;
@@ -10,46 +11,73 @@
         background-color: rgba(255, 255, 255, 0.25);
         backdrop-filter: blur(5px);
       "
-                class="mb-2"
-        >
-            <b-card-img
-                    v-bind:src="img"
-                    alt="Image"
-                    height="130"
-                    width="130"
-                    class="mb-4"
-            />
-            <p>{{ category }}</p>
-            <p>{{ company }} : {{ device_type }}</p>
-            <div v-if="company === 'tp-link'">
-                <toggle-button v-model="device_on" @change="onChangeEventHandler"/>
-
-                <!--                <b-button variant="success" @click="turnOnDevice">On</b-button>-->
-                <!--                <b-button @click="turnOffDevice()">Off</b-button>-->
-                <input
-                        v-if="device_type == 'L510E'"
-                        type="range"
-                        min="1"
-                        max="100"
-                        v-model="brightness"
-                        @change="brightnessChanged()"
+                    class="mb-2"
+            >
+                <b-card-img
+                        v-bind:src="img"
+                        alt="Image"
+                        height="130"
+                        width="130"
+                        class="mb-4"
                 />
-                <p>
-                    Status: {{ this.dev.device_info.device_on ? "On" : "Off" }}
-                </p>
-            </div>
-            <div v-if="device_type === 'RGB_LED_STRIP'" style="text-align: center">
-                <b-button pill @click="onRGBClick()" center>
-                    <verte v-model="color" picker="wheel" model="rgb"/>
-                </b-button>
-            </div>
-            <div slot="footer">
-                <b-button style="background-color: #4287f5" v-bind:to="navigate()"
-                >Information
-                </b-button>
-                <b-button variant="danger" @click="deleteDevice()">X</b-button>
-            </div>
-        </b-card>
+                <p>{{ category }}</p>
+                <p>{{ company }} : {{ device_type }}</p>
+                <div v-if="company === 'tp-link'">
+                    <toggle-button v-model="device_on" @change="onChangeEventHandler"/>
+
+                    <!--                <b-button variant="success" @click="turnOnDevice">On</b-button>-->
+                    <!--                <b-button @click="turnOffDevice()">Off</b-button>-->
+                    <input
+                            v-if="device_type == 'L510E'"
+                            type="range"
+                            min="1"
+                            max="100"
+                            v-model="brightness"
+                            @change="brightnessChanged()"
+                    />
+                    <p>
+                        Status: {{ this.dev.device_info.device_on ? "On" : "Off" }}
+                    </p>
+                </div>
+                <div v-if="device_type === 'RGB_LED_STRIP'" style="text-align: center">
+                    <b-button pill @click="onRGBClick()" center>
+                        <verte v-model="color" picker="wheel" model="rgb"/>
+                    </b-button>
+                </div>
+                <div slot="footer">
+                    <b-button style="background-color: #4287f5" v-bind:to="navigate()"
+                    >Information
+                    </b-button>
+                    <b-button variant="danger" @click="deleteDevice()">X</b-button>
+                </div>
+            </b-card>
+        </div>
+        <div v-else>
+            <b-card
+                    v-bind:title="name"
+                    style="
+        max-width: 540px;
+        min-width: 200px;
+        min-height: 425px;
+        max-height: 500px;
+        background-color: rgba(255, 255, 255, 0.25);
+        backdrop-filter: blur(5px);
+      "
+                    class="mb-2"
+            >
+                <b-card-img
+                        v-bind:src="img"
+                        alt="Image"
+                        height="130"
+                        width="130"
+                        class="mb-4"
+                />
+                <p>{{ category }}</p>
+                <p>{{ company }} : {{ device_type }}</p>
+                <div style="color:#ff0033">Could not load this device. Check the backend</div>
+
+            </b-card>
+        </div>
     </div>
     <div v-else>
         <h3>Loading results...</h3>
@@ -154,8 +182,12 @@
       if (this.company === "tp-link") {
         // await TapoService.wakeTapoDevice(this.id);
         const deviceResult = await TapoService.fetchTapoDevice(this.id);
-        this.dev = deviceResult.device;
-        this.device_on = this.dev.device_info.device_on;
+        if (deviceResult === "couldn't retrieve tapo device info\n") {
+          this.dev = null;
+        } else {
+          this.dev = deviceResult.device;
+          this.device_on = this.dev.device_info.device_on;
+        }
         this.state = "loaded";
       } else {
         this.state = "loaded";
