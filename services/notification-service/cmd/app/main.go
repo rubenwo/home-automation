5031a53a382d150c1ad7d5fb1e7dfec30c896ab9
Ruben <rubenwoldhuis@gmail.com>
2021-09-07 21:21:39 +0200

    [Fixed] oopsie

--- services/notification-service/cmd/app/main.go
+++ services/notification-service/cmd/app/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/messaging"
+	"fmt"
 	"github.com/rubenwo/home-automation/servics/notification-service/internal/service"
+	"google.golang.org/api/option"
 	"log"
 	"net/http"
 )
@@ -12,16 +17,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//opt := option.WithCredentialsFile("./service-account-file.json")
-	//app, err := firebase.NewApp(context.Background(), nil, opt)
-	//if err != nil {
-	//	log.Fatalf("error initializing app: %v\n", err)
-	//}
-	//msgClient, err := app.Messaging(context.Background())
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	opt := option.WithCredentialsFile("./service-account-file.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	msgClient, err := app.Messaging(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	msg, err := msgClient.Send(context.Background(), &messaging.Message{
+		Token: "blah",
+		Notification: &messaging.Notification{
+			Title: "Hello World",
+			Body:  "Bla",
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(msg)
 
 	router, err := service.New(cfg)
 	if err != nil {
--- services/notification-service/go.mod
+++ services/notification-service/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	cloud.google.com/go v0.94.1 // indirect
+	cloud.google.com/go/storage v1.16.1 // indirect
 	firebase.google.com/go/v4 v4.6.0
 	github.com/go-chi/chi v1.5.4
 	github.com/go-pg/pg/v10 v10.10.5
@@ -13,6 +14,6 @@ require (
 	golang.org/x/net v0.0.0-20210903162142-ad29c8ab022f // indirect
 	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/api v0.56.0 // indirect
+	google.golang.org/api v0.56.0
 	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83 // indirect
 )
